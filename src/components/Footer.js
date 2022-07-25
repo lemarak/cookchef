bfaff14f17b90386d5b4228cee321fb29d7a785a
Stéphane Dauguet <sdauguet@gmail.com>
2022-07-25 18:01:58 +0200

    header and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,13 @@
+import styles from "./Footer.module.scss";
+
 const Footer = () => {
-  return <div className="b3">Footer</div>;
+  return (
+    <footer
+      className={`${styles.footer} d-flex flex-row align-items-center justify-content-center p-20`}
+    >
+      <p>Copyright © 2022 Cookchef Stéphane, Inc.</p>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,24 @@
+import styles from "./Header.module.scss";
+import cookchef from "../assets/images/cookchef.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faBasketShopping } from "@fortawesome/free-solid-svg-icons";
+
 const Header = () => {
-  return <div className="b1">Header</div>;
+  return (
+    <header className={`${styles.header} d-flex flex-row align-items-center`}>
+      <FontAwesomeIcon icon={faBars} className="mr-15" />
+      <div className="flex-fill">
+        <img src={cookchef} alt="logo cookchef" />
+      </div>
+      <ul>
+        <button className="mr-5 btn btn-reverse-primary">
+          <FontAwesomeIcon icon={faBasketShopping} className="mr-5" />
+          <span>panier</span>
+        </button>
+        <button className="btn btn-primary">connexion</button>
+      </ul>
+    </header>
+  );
 };
 
 export default Header;
