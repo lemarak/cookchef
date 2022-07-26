c8c63eb0475f028d2ed07a45668058cffb07d0a8
Stéphane Dauguet <sdauguet@gmail.com>
2022-07-26 10:33:28 +0200

    add css content

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,5 +1,23 @@
+import styles from "./Content.module.scss";
+
 const Content = () => {
-  return <div className="b2 flex-fill">Content</div>;
+  return (
+    <div className="flex-fill container p-20">
+      <h1 className="my-30">Découvrez nos nouvelles recettes</h1>
+      <div className={`${styles.contentCard} card p-20`}>
+        <div className={styles.grid}>
+          <div className="styles.elem">Element</div>
+          <div className="styles.elem">Element</div>
+          <div className="styles.elem">Element</div>
+          <div className="styles.elem">Element</div>
+          <div className="styles.elem">Element</div>
+          <div className="styles.elem">Element</div>
+          <div className="styles.elem">Element</div>
+          <div className="styles.elem">Element</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Content;
